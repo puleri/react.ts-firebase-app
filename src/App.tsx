1b48e1ee189f0b15cd1d744cf7476895b40314ad
mp4 <puleri.mp4@gmail.com>
2021-11-10 13:55:03 -0800

    Fixed error where depreceated route broke BrowserRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,30 @@
-import { FC } from 'react';
-import { Route } from 'react-router-dom';
+import { FC, useState } from 'react';
+import { BrowserRouter as Router,
+  Routes,
+  Route,
+ } from 'react-router-dom';
 import Hello from './components/Hello/Hello';
 import './App.css';
 
+export interface RouteProps {
+  props: {
+    path?: string;
+    default?: boolean;
+    exact?: boolean;
+  }
+}
+
 const App:FC = () => {
+
+
   return (
-    <div className="App">
-      <Route
-        path="/"
-        >
-        <Hello />
-      </Route>
-    </div>
+    <Router>
+      <Routes>
+
+          <Route element={<Hello />} path="/"/>
+
+      </Routes>
+    </Router>
   );
 }
 
