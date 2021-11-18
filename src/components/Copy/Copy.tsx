8b65bea29ed46c97bcbca01a945c09116fd4baa0
mp4 <puleri.mp4@gmail.com>
2021-11-18 12:48:42 -0800

    Added API key generator on Copy to Clipboard component

--- src/components/Copy/Copy.tsx
+++ src/components/Copy/Copy.tsx
@@ -5,6 +5,8 @@ import './Copy.css';
 const Copy:FC = () => {
 
   const [copySuccess, setCopySuccess] = useState('Copy to clipboard');
+  const [apiKey, setApiKey] = useState('xxxxxxxx-xxxx-xxxx-yxxx-xxxxxxxxxxxx');
+
 
   const textAreaRef:any = useRef(null)
 
@@ -17,19 +19,40 @@ const Copy:FC = () => {
      setTimeout(() => setCopySuccess('Copy to clipboard'), 3000)
   }
 
+  function generateUUID()
+{
+	var d = new Date().getTime();
+
+	if( window.performance && typeof window.performance.now === "function" )
+	{
+		d += performance.now();
+	}
+
+	var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c)
+	{
+		var r = (d + Math.random()*16)%16 | 0;
+		d = Math.floor(d/16);
+		return (c=='x' ? r : (r&0x3|0x8)).toString(16);
+	});
+
+return setApiKey(uuid);
+}
+
   return (
     <>
       <div className="copy-container">
+        <button className="hover-underline-animation" id="api-gen" onClick={() => generateUUID()}>API Key Generator</button>
         <ReactToolTip
         id="copy"
-        place={"right"}
+        place={"top"}
         type={'dark'}
         effect={'solid'}
         multiline={true}
         >
         {copySuccess}
         </ReactToolTip>
-        <h3>Code: <span data-tip data-for='copy' onClick={() => copyToClip()} ref={textAreaRef} id="code">4444</span></h3>
+        <h3>API Key: <span data-tip data-for='copy' onClick={() => copyToClip()} ref={textAreaRef} id="code">{apiKey}</span></h3>
+        <br/>
       </div>
     </>
   )
